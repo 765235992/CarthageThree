b9c24a43355074c741d1628f352e2caecd256920
765235992 <765235992@users.noreply.github.com>
2017-07-01 00:34:14 +0800

    3
    3

---
+++ Carthage/Build/iOS/MyCarthage.framework/Headers/DJPeople.h
@@ -0,0 +1,13 @@
+//
+//  DJPeople.h
+//  CarthageThree
+//
+//  Created by Jone on 2017/7/1.
+//  Copyright © 2017年 河南东京Web俱乐部. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DJPeople : NSObject
+- (void)eat;
+@end
