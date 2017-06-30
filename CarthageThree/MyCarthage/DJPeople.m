83d462fc969931fc35689fea58fbe442abb53910
765235992 <765235992@users.noreply.github.com>
2017-07-01 00:23:43 +0800

    1
    1

---
+++ CarthageThree/MyCarthage/DJPeople.m
@@ -0,0 +1,17 @@
+//
+//  DJPeople.m
+//  CarthageThree
+//
+//  Created by Jone on 2017/7/1.
+//  Copyright © 2017年 河南东京Web俱乐部. All rights reserved.
+//
+
+#import "DJPeople.h"
+
+@implementation DJPeople
+- (void)eat
+{
+    NSLog(@"我爱吃香蕉");
+}
+
+@end
---
+++ CarthageThree/MyCarthage/MyCarthage.h
@@ -0,0 +1,14 @@
+//
+//  MyCarthage.h
+//  CarthageThree
+//
+//  Created by Jone on 2017/7/1.
+//  Copyright © 2017年 河南东京Web俱乐部. All rights reserved.
+//
+
+#ifndef MyCarthage_h
+#define MyCarthage_h
+#import "DJPeople.h"
+
+
+#endif /* MyCarthage_h */
